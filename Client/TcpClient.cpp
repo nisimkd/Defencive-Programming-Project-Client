7b9df501e9ce43fa18bb47ac066a897afd5facc2
Nisim Kadosh <nisimkd1@gmail.com>
2022-01-26 23:57:13 +0200

    Success Serialize data and send to Server and Deserialize data from the server.

--- Client/TcpClient.cpp
+++ Client/TcpClient.cpp
@@ -15,11 +15,52 @@ TcpClient::TcpClient(std::string serverAddress, std::string serverPort)
 }
 */
 
+#pragma pack(push, 1)
+struct clientMessageData
+{
+	char clientId[16];
+	uint8_t version;
+	uint16_t code;
+	uint32_t payloadSize;
+
+	char name[255];
+};
+#pragma pack(pop)
+
+union ClientMessage
+{
+	clientMessageData data;
+	char buffer[sizeof(clientMessageData)];
+};
+
+#pragma pack(push, 1)
+struct serverMessageData
+{
+	uint8_t version;
+	uint16_t code;
+	uint32_t payloadSize;
+
+	char response[255];
+};
+#pragma pack(pop)
+
 void initMessage(char message[], int length);
 
 std::string sendRequestToServer(std::string serverAddress, std::string serverPort, std::string messageRequest)
 {
 	const int max_length = 1024;
+
+	ClientMessage m;
+	m.data.version = 1;
+	m.data.code = 1100;
+	m.data.payloadSize = 255;
+	
+	initMessage(m.data.name, 255);
+	memcpy(m.data.name, messageRequest.c_str(), messageRequest.length());
+	
+	char buf[16 + sizeof(uint8_t) + sizeof(uint16_t) + sizeof(uint32_t) + 255];
+
+	memcpy(buf, m.buffer, 16 + sizeof(uint8_t) + sizeof(uint16_t) + sizeof(uint32_t) + 255);
 
 	try
 	{
@@ -29,16 +70,16 @@ std::string sendRequestToServer(std::string serverAddress, std::string serverPor
 		boost::asio::connect(s, resolver.resolve(serverAddress, serverPort));
 
 		for (; ; ) 
-		{
-			char request[max_length];
-			initMessage(request, max_length);
-			memcpy(request, messageRequest.c_str(), messageRequest.length());
-			boost::asio::write(s, boost::asio::buffer(request, max_length));
+		{			
+			boost::asio::write(s, boost::asio::buffer(buf, 1024));
 			char reply[max_length];
+			serverMessageData serverMessage;
 			size_t reply_length =
 				boost::asio::read(s, boost::asio::buffer(reply, max_length));
 
-			std::cout << "Reply is: " << reply << "\n";
+			memcpy(&serverMessage, reply, sizeof(serverMessageData));
+
+			std::cout << "Version: " << (int)serverMessage.version << ", Code: " << serverMessage.code << ", Payload Size: " << serverMessage.payloadSize << ", Response: " << serverMessage.response << std::endl;
 
 			return std::string(reply);
 		}
